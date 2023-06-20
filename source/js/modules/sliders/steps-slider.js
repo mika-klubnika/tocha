d58848760cbe3e102ada074a566e206cccb7d0a9
marina migunova <mika-klubnika@yandex.ru>
2023-06-20 16:37:23 +0300

    добавляет блок шаги

---
+++ source/js/modules/sliders/steps-slider.js
@@ -0,0 +1,46 @@
+/* globals Swiper */
+export const initStepsSlider = () => {
+
+  const stepsSlider = document.querySelector('[data-steps-slider]');
+
+  if (!stepsSlider) {
+    return;
+  }
+
+  const swiper = new Swiper(stepsSlider, {
+    watchOverflow: true,
+    roundLengths: true,
+    speed: 400,
+    slidesPerView: 'auto',
+    grabCursor: true,
+
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+    },
+
+    breakpoints: {
+      0: {
+        spaceBetween: 20,
+        slidesOffsetBefore: 22,
+        slidesOffsetAfter: 22,
+
+
+      },
+      768: {
+        spaceBetween: 20,
+        slidesOffsetBefore: 40,
+        slidesOffsetAfter: 40,
+      },
+      1200: {
+        spaceBetween: 27,
+        slidesOffsetBefore: 0,
+        slidesOffsetAfter: 0,
+
+      },
+      1440: {
+        spaceBetween: 173,
+      },
+    },
+  });
+};
